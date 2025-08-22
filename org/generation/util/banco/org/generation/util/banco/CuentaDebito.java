04c4938500de35c8f40752594f6ba038d3871d76
Mayra Estrada <mayra.espr@gmail.com>
2025-08-22 11:25:07 -0600

    Se agrega clase CuentaDebito

---
+++ org/generation/util/banco/org/generation/util/banco/CuentaDebito.java
@@ -0,0 +1,49 @@
+package org.generation.util.banco;
+
+public abstract class CuentaDebito {
+    private double montoMinimo;
+    private double saldo;
+    
+	public CuentaDebito(double montoMinimo, double saldo) {
+		super();
+		this.montoMinimo = montoMinimo;
+		this.saldo = saldo;
+	}
+
+	public double getMontoMinimo() {
+		return montoMinimo;
+	}//getMontoMinimo
+
+	public void setMontoMinimo(double montoMinimo) {
+		this.montoMinimo = montoMinimo;
+	}//setMontoMinimo
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public double retiro(double cantidad) {
+        if (cantidad <= 0) {
+            System.out.println("La cantidad a retirar debe ser mayor a $100");
+            return saldo;
+        }//if
+        if (saldo >= cantidad) {
+            saldo -= cantidad;
+            System.out.println("Retiro exitoso: " + cantidad);
+        } else {
+            System.out.println("Saldo insuficiente");
+        }//else
+        return saldo;
+    }//retiro
+	
+	public double deposito(double cantidad) {
+        if (cantidad <= 0) {
+            System.out.println("La cantidad a depositar debe ser mayor a cero.");
+            return saldo;
+        }//if
+        saldo += cantidad;
+        System.out.println("Depósito exitoso: " + cantidad);
+        return saldo;
+    }//deposito
+	
+}//class
